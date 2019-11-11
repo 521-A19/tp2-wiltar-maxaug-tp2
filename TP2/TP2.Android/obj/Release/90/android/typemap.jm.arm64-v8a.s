8938d173612f278b2d92f0134322969f67a39734
maxaug999 <maxaug999@hotmail.com>
2019-11-11 14:11:37 -0500

    ajout tests + gitignore

---
+++ TP2/TP2.Android/obj/Release/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 828f6dd5eb5798ca5061f4d5f3b008d9f27e66f4 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	761
+	/* entry-length */
+	.word	232
+	/* value-offset */
+	.word	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 176553
+	.include	"typemap.jm.inc"
---
+++ TP2/TP2.Android/obj/Release/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 286515b4f7391d422be6c75169a6e87b91da57f8 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	873
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 202537
+	.include	"typemap.mj.inc"
